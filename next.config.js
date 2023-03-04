e82293cee707ace5340a2a90996e44937e06c488
Vladyslav Kyiashko <kiyashko.vlad1@gmail.com>
2023-03-05 00:33:22 +0100

    ci: add redirect to home page

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
