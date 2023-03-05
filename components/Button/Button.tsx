3799dfc8e0defeea7d30196338a46db6158068b8
Vladyslav Kyiashko <kiyashko.vlad1@gmail.com>
2023-03-05 02:46:14 +0100

    feat: add Button variant prop

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,39 +1,66 @@
 import { ButtonHTMLAttributes } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-type ButtonVariant = "primary" | "secondary" | "danger";
+type ButtonColor = "primary" | "secondary" | "danger";
+type ButtonVariant = "contained" | "outlined";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: ButtonColor;
   variant?: ButtonVariant;
 }
 
-export function Button({
-  children,
-  variant = "primary",
-  ...rest
-}: ButtonProps) {
+export function Button(props: ButtonProps) {
+  const { children, color = "primary", variant = "contained", ...rest } = props;
+
   return (
-    <StyledButton $variant={variant} {...rest}>
+    <StyledButton $color={color} $variant={variant} {...rest}>
       {children}
     </StyledButton>
   );
 }
 
-interface StyledButtonProps {
+interface ButtonColorProp {
+  $color: ButtonColor;
+}
+
+interface StyledButtonProps extends ButtonColorProp {
   $variant: ButtonVariant;
 }
 
-const StyledButton = styled.button<StyledButtonProps>`
-  background-color: ${({ theme, $variant }) => theme[$variant].main};
-  padding: 0.5rem 1rem;
-  border: unset;
-  border-radius: 0.5rem;
+const ContainedButtonCss = css<ButtonColorProp>`
+  background-color: ${({ theme, $color }) => theme[$color].main};
   color: ${({ theme }) => theme.text.secondary};
-  font-size: 0.875rem;
-
-  cursor: pointer;
+  border: none;
 
   &:hover {
-    background-color: ${({ theme, $variant }) => theme[$variant].dark};
+    background-color: ${({ theme, $color }) => theme[$color].dark};
   }
+`;
+
+const OutlinedButtonCss = css<ButtonColorProp>`
+  background-color: transparent;
+  color: ${({ theme, $color }) => theme[$color].main};
+  border: ${({ theme, $color }) => `2px solid ${theme[$color].main}`};
+
+  &:hover {
+    background-color: ${({ theme, $color }) => theme[$color].light};
+  }
+`;
+
+const StyledButton = styled.button<StyledButtonProps>`
+  ${({ $variant }) => $variant === "contained" && ContainedButtonCss}
+  ${({ $variant }) => $variant === "outlined" && OutlinedButtonCss}
+  
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+
+  cursor: pointer;
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
 `;
