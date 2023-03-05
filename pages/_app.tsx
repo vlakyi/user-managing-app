cf3936067faffb5e7a0da00c1af2c47a80e25777
Vladyslav Kyiashko <kiyashko.vlad1@gmail.com>
2023-03-05 13:08:17 +0100

    fix: add react-tostify styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
 import Head from "next/head";
